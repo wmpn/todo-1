0d14a751efb6ecfa6fd56a7772912d9884a34ce3
Praveen Nirmal <wmpnga@gmail.com>
2021-05-17 12:15:14 +0530

    added the function About

---
+++ src/about.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>This is the about page</h1>
+        </div>
+    );
+}
+
+export default About;
