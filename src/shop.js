2ff664f68e9552793e6bab1ba5ad3d940e0373b6
Praveen Nirmal <wmpnga@gmail.com>
2021-05-17 12:15:41 +0530

    added the functiion Shop

---
+++ src/shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Shop = () => {
+    return (
+        <div className="shop">
+            <h1>
+                This is the shopping page
+            </h1>
+        </div>
+    );
+}
+
+export default Shop;
